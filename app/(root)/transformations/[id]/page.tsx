323ef47dddeddb4b538db68579b2ec8f55a15093
thang <nguyenhuuthang02032003@gmail.com>
2024-02-27 09:53:05 +0700

    initital commit

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Transformation = () => {
+  return (
+    <div>Transformation</div>
+  )
+}
+
+export default Transformation
+
+// localhost:3000/transformations
+// localhost:3000/transformations/[id]
---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,17 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = new Schema ({
+  clerkId: {type: String, require: true, unique: true },
+  email: {type: String, require: true, unique: true },
+  username: {type: String, require: true, unique: true },
+  photo: {type: String, require: true },
+  firstName: {type: String },
+  lastName: {type: String },
+  planId: {type: Number, default: 1 },
+  creditBalnce: {type: Number, default: 10 },
+  
+});
+
+const User = models?.User || model('User',UserSchema);
+
+export default User
